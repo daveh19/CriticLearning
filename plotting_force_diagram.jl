24e7eacb8e434f9af9bacc83f01fec37d7a0cccb
David Higgins (TUPro) <dave@uiginn.com>
2015-04-28 17:21:27 +0200

    Plotting in matplotlib is row-major but I'm using column major addressing, corrected the plots.

--- plotting_force_diagram.jl
+++ plotting_force_diagram.jl
@@ -1,49 +1,72 @@
 using PyPlot;
 using Distributions;
 
-include("inverse_cdf.jl");
+#include("inverse_cdf.jl");
 
 no_points = 101;
+#no_points = 10;
+#no_y_points = no_points - 1; # this is to ensure that I plot the vector field in the right direction, julia is column major but matplot lib is row major which causes confusion! 
+no_y_points = no_points;
 p = linspace(0, 1, no_points);
-
+p_y = linspace(0, 1, no_y_points);
 
 dist_pdf(x) = pdf(Normal(0,1), x);
 dist_cdf(x) = cdf(Normal(0,1), x);
 #inv_cdf(x) = 1.0 / cdf(Normal(0,1), x); #Not 1/fn but inverse function!!
 invphi(p) = sqrt(2) * erfinv(2 * p - 1.0)
 
-xa_norm_sq = 1.0;
+xa_norm_sq = 10.0;
 
-rho = zeros(no_points,no_points);
+rho = zeros(no_points,no_y_points);
 for i = 1:no_points
-	for j = 1:no_points
+	for j = 1:(no_y_points)
 		rho[i,j] = (p[i] - p[j]) / 2.0;
 	end
 end
 
-deriv_p_a = zeros(no_points, no_points);
-deriv_p_b = zeros(no_points, no_points);
+deriv_p_a = zeros(no_points, no_y_points);
+deriv_p_b = zeros(no_points, no_y_points);
 for i = 1:no_points
-	for j = 1:no_points
+	for j = 1:(no_y_points)
+		# -ve for task B is for the opposite sign on rho
 		# unbounded post firing rate equations
 		#=deriv_p_a[i,j] = dist_pdf(invnorm(p[i])) * xa_norm_sq * rho[i,j] * invnorm(p[i]);
-		deriv_p_b[i,j] = - dist_pdf(invnorm(p[j])) * xa_norm_sq * rho[i,j] * invnorm(p[j]);=#
+		deriv_p_b[i,j] = dist_pdf(invnorm(p[j])) * xa_norm_sq * (-rho[i,j]) * invnorm(p[j]);=#
 		# binary output neurons
 		#=deriv_p_a[i,j] = dist_pdf(invnorm(p[i])) * xa_norm_sq * rho[i,j] * p[i];
 		deriv_p_b[i,j] = - dist_pdf(invnorm(p[j])) * xa_norm_sq * rho[i,j] * p[j];=#
 		# binary output neurons - alternative
 		#=deriv_p_a[i,j] = dist_pdf(invnorm(p[i])) * xa_norm_sq * rho[i,j] * p[i] * (1-p[i]);
 		deriv_p_b[i,j] = - dist_pdf(invnorm(p[j])) * xa_norm_sq * rho[i,j] * p[j] * (1-p[j]);=#
+		# binary output neurons - new (in first write-up)
+		deriv_p_a[i,j] = dist_pdf(invnorm(p[i])) * xa_norm_sq * rho[i,j] * (2 * p[i] - 1);
+		deriv_p_b[i,j] = dist_pdf(invnorm(p[j])) * xa_norm_sq * (-rho[i,j]) * (2 * p[j] - 1);
 		# wta binary output for principal choice, second choice is normalised by magnitude of first
-		deriv_p_a[i,j] = dist_pdf(invnorm(p[i])) * xa_norm_sq * rho[i,j] * (p[i] + (1-p[i]) * p[i]);
-		deriv_p_b[i,j] = - dist_pdf(invnorm(p[j])) * xa_norm_sq * rho[i,j] * (p[j] + (1-p[j]) * p[j]);
+		#=deriv_p_a[i,j] = dist_pdf(invnorm(p[i])) * xa_norm_sq * rho[i,j] * (p[i] + (1-p[i]) * p[i]);
+		deriv_p_b[i,j] = - dist_pdf(invnorm(p[j])) * xa_norm_sq * rho[i,j] * (p[j] + (1-p[j]) * p[j]);=#
+		# Rescaled outputs - new (in first write-up)
+		#=deriv_p_a[i,j] = dist_pdf(invnorm(p[i])) * xa_norm_sq * rho[i,j] * ( (2 * p[i] - 1) * (1 - p[i]) );
+		deriv_p_b[i,j] = dist_pdf(invnorm(p[j])) * xa_norm_sq * (-rho[i,j]) * ( (2 * p[j] - 1) * (1 - p[j]) );=#
+		# basic flow field
+		#deriv_p_a[i,j] = xa_norm_sq * rho[i,j] * (2 * p[i] - 1);
+		#deriv_p_b[i,j] = xa_norm_sq * (-rho[i,j]) * (2 * p[j] - 1);
 	end
 end
 
-figure();
-quiver(p,p,deriv_p_a, deriv_p_b);
-savefig("vector_field_scaled_output.pdf");
+print("Plotting...\n")
+filename_change = "unbounded_post"
+filename_change = "binary_new"
+#filename_change = "rescaled_new"
+filename_base = string("vector_field_", filename_change);
+filename_quiver = string("quiver_",filename_base,".pdf")
+filename_stream = string("stream_",filename_base,".pdf")
 
 figure();
-streamplot(p,p,deriv_p_a,deriv_p_b)
-savefig("quiver_vector_field_output.pdf");
+quiver(p,p,deriv_p_a', deriv_p_b');
+#quiver(p,p_y,deriv_p_a', deriv_p_b');
+savefig(filename_quiver);
+
+figure();
+streamplot(p,p,deriv_p_a',deriv_p_b')
+#streamplot(p,p_y,deriv_p_a',deriv_p_b')
+savefig(filename_stream);
