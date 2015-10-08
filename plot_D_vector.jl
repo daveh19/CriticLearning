ca66f163cffb285b122e4464ae64ca914fd3a8e9
David Higgins (TUPro) <dave@uiginn.com>
2015-10-08 09:36:35 +0200

    Major code tidy up. Removed all non-current code from plotting of linear outputs flow.

--- plot_D_vector.jl
+++ plot_D_vector.jl
@@ -38,49 +38,13 @@ deriv_D_a = zeros(no_points, no_y_points);
 deriv_D_b = zeros(no_points, no_y_points);
 
 
-## Reward learning component to flow is an option in probabilistic diagram
-plot_unbiased_learning = true :: Bool;
-# Success signal is necessary for unbiased learning component
-# constant Success signal
-#S_1 = +1.0 / 10.0;
-#S_2 = -1.0 / 10.0;
-# linear decay of Success signal
-#S_1 = 1.0 - p;
-#S_2 = -p - 1.0;
-# Stationary Success signal
-S_1 = 1.0 - (2*p-1.0);
-S_2 = -1.0 - (2*p-1.0);
-
-## Rho represents error in estimation of Success signal, used in probabilistic calculation
-rho_a = zeros(no_points,no_y_points);
-for i = 1:no_points
-	for j = 1:(no_y_points)
-		rho_a[i,j] = (p[i] - p[j]);
-		#rho_a[i,j] = p[i] - ((p[i] + p[j] + 2.0 * p_ext) / 4.0);
-		#rho_a[i,j] = p[i] - ((p[j] + p_ext) / 2.0);
-	end
-end
-rho_b = zeros(no_points,no_y_points);
-for i = 1:no_points
-	for j = 1:(no_y_points)
-		rho_b[i,j] = (p[j] - p[i]);
-		#rho_b[i,j] = p[j] - ((p[i] + p[j] + 2.0 * p_ext) / 4.0);
-		#rho_b[i,j] = p[j] - ((p[i] + p_ext) / 2.0);
-	end
-end
-
-
-## Constants
-xa_norm_sq = 1.0;
-p_ext = 0.30;
-
 # Confusion parameter
 critic_dimensions = 2;
-c = 1 / critic_dimensions; # currently equal confusion mix of all true critics
+#=c = 1 / critic_dimensions; # currently equal confusion mix of all true critics
 C = ones(critic_dimensions,critic_dimensions)
 C *= c
 #C = eye(critic_dimensions) # perfect critic
-A = eye(critic_dimensions) - C
+A = eye(critic_dimensions) - C=#
 
 # Probabilistic presentation of individual tasks
 prob_task = ones(1,critic_dimensions);
@@ -112,15 +76,6 @@ for i = 1:no_points
 		#
 		# Calculation of change of difference in outputs
 		#
-		# for R^{true} = p
-		# temp_a = sigma^2 * pdf(Normal(0,sigma), d_a[i]);
-		# temp_b = sigma^2 * pdf(Normal(0,sigma), d_b[j]);
-		# # equations for R^{true} = p
-		# temp_a += A[1,1] * cdf(Normal(0,sigma), d_a[i]) * d_a[i];
-		# temp_a += A[1,2] * cdf(Normal(0,sigma), d_b[j]) * d_a[i];
-		# temp_b += A[2,1] * cdf(Normal(0,sigma), d_a[i]) * d_b[j];
-		# temp_b += A[2,2] * cdf(Normal(0,sigma), d_b[j]) * d_b[j];
-
 		# *2 for R^{true} = (2p-1)
 		temp_a = sigma^2 * pdf(Normal(0,sigma), (d_a[i])) * 2;
 		temp_b = sigma^2 * pdf(Normal(0,sigma), (d_b[j])) * 2;
@@ -161,23 +116,6 @@ for i = 1:no_points
 		#
 		# Calculation of change of probability of outcome
 		#
-		# -ve for task B is for the opposite sign on rho
-		# unbounded post firing rate equations
-		#=deriv_p_a[i,j] = dist_pdf(invnorm(p[i])) * xa_norm_sq * rho[i,j] * invnorm(p[i]);
-		deriv_p_b[i,j] = dist_pdf(invnorm(p[j])) * xa_norm_sq * (-rho[i,j]) * invnorm(p[j]);=#
-		# binary output neurons
-		#=deriv_p_a[i,j] = dist_pdf(invnorm(p[i])) * xa_norm_sq * rho[i,j] * p[i];
-		deriv_p_b[i,j] = - dist_pdf(invnorm(p[j])) * xa_norm_sq * rho[i,j] * p[j];=#
-		# binary output neurons - alternative
-		#=deriv_p_a[i,j] = dist_pdf(invnorm(p[i])) * xa_norm_sq * rho[i,j] * p[i] * (1-p[i]);
-		deriv_p_b[i,j] = - dist_pdf(invnorm(p[j])) * xa_norm_sq * rho[i,j] * p[j] * (1-p[j]);=#
-
-		# binary output neurons - new (in first write-up)
-		# effects of unsupervised bias
-		#deriv_p_a[i,j] = dist_pdf(invnorm(p[i])) * xa_norm_sq * rho_a[i,j] * (2 * p[i] - 1);
-		#deriv_p_b[i,j] = dist_pdf(invnorm(p[j])) * xa_norm_sq * rho_b[i,j] * (2 * p[j] - 1);
-
-
 		Da[i] = invphi(p[i]);
 		Db[j] = invphi(p_y[j]);
 		p_temp_a = sigma^2 * pdf(Normal(0,sigma), Da[i]) * 2;
@@ -212,24 +150,6 @@ for i = 1:no_points
 
 		deriv_p_a[i,j] = pdf(Normal(0,sigma), Da[i]) * p_deriv_D_a[i,j];
 		deriv_p_b[i,j] = pdf(Normal(0,sigma), Db[j]) * p_deriv_D_b[i,j];
-
-
-		#=if(plot_unbiased_learning)
-			# include effects of regular learning signal
-			#deriv_p_a[i,j] = dist_pdf(invnorm(p[i])) * xa_norm_sq * ( ( p[i]*S_1[i] - (1-p[i])*S_2[i] ) + ( rho_a[i,j] * (2 * p[i] - 1) ) );
-			#deriv_p_b[i,j] = dist_pdf(invnorm(p[j])) * xa_norm_sq * ( ( p[j]*S_1[j] - (1-p[j])*S_2[j] ) + ( rho_b[i,j] * (2 * p[j] - 1) ) );
-			deriv_p_a[i,j] += dist_pdf(invnorm(p[i])) * xa_norm_sq * ( ( p[i]*S_1[i] - (1-p[i])*S_2[i] ) );
-			deriv_p_b[i,j] += dist_pdf(invnorm(p[j])) * xa_norm_sq * ( ( p[j]*S_1[j] - (1-p[j])*S_2[j] ) );
-		end=#
-		# wta binary output for principal choice, second choice is normalised by magnitude of first
-		#=deriv_p_a[i,j] = dist_pdf(invnorm(p[i])) * xa_norm_sq * rho[i,j] * (p[i] + (1-p[i]) * p[i]);
-		deriv_p_b[i,j] = - dist_pdf(invnorm(p[j])) * xa_norm_sq * rho[i,j] * (p[j] + (1-p[j]) * p[j]);=#
-		# Rescaled outputs - new (in first write-up)
-		#=deriv_p_a[i,j] = dist_pdf(invnorm(p[i])) * xa_norm_sq * rho[i,j] * ( (2 * p[i] - 1) * (1 - p[i]) );
-		deriv_p_b[i,j] = dist_pdf(invnorm(p[j])) * xa_norm_sq * (-rho[i,j]) * ( (2 * p[j] - 1) * (1 - p[j]) );=#
-		# basic flow field
-		#deriv_p_a[i,j] = xa_norm_sq * rho[i,j] * (2 * p[i] - 1);
-		#deriv_p_b[i,j] = xa_norm_sq * (-rho[i,j]) * (2 * p[j] - 1);
 	end
 end
 
