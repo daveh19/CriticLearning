7d5dc1a187433a01b0261fa23a8b05157256ad66
David Higgins (TUPro) <dave@uiginn.com>
2015-10-08 19:09:34 +0200

    Added plotting over D-positive for binary outputs.

--- plot_Dbinary_vector.jl
+++ plot_Dbinary_vector.jl
@@ -13,7 +13,7 @@ invphi(p) = sqrt(2) * erfinv(2 * p - 1.0)
 
 ## Plotting over D, D~ (+ve), and p optional
 use_plot_over_D_pos = true :: Bool;
-use_plot_over_D = false :: Bool;
+use_plot_over_D = true :: Bool;
 use_plot_over_p = true :: Bool;
 use_add_trajectories_to_plot = false :: Bool;
 
@@ -96,14 +96,14 @@ for i = 1:no_points
 		# positive association in plotting of D with reward (use d_a as d_a^~)
 		if (use_plot_over_D_pos)
 			# *2 for R^{true} = (2p-1)
-			temp_a = 4 * cdf(Normal(0,sigma),d_a[i]) * (1 - cdf(Normal(0,sigma),d_a[i]))
-			temp_b = 4 * cdf(Normal(0,sigma),d_b[j]) * (1 - cdf(Normal(0,sigma),d_b[j]))
+			temp_a = 4 * cdf(Normal(0,sigma), (d_a[i]) ) * (1 - cdf(Normal(0,sigma), (d_a[i]) ) )
+			temp_b = 4 * cdf(Normal(0,sigma), (d_b[j]) ) * (1 - cdf(Normal(0,sigma), (d_b[j]) ) )
 			# equations for R^{true} = (2p-1)
-			temp_a += A[1,1] * (2 * cdf(Normal(0,sigma), d_a[i]) - 1) * (2 * cdf(Normal(0,sigma),d_a[i]) - 1);
-			temp_a += A[1,2] * (2 * cdf(Normal(0,sigma), d_b[j]) - 1) * (2 * cdf(Normal(0,sigma),d_a[i]) - 1);
+			temp_a += A[1,1] * (2 * cdf(Normal(0,sigma), (d_a[i]) ) - 1) * (2 * cdf(Normal(0,sigma), (d_a[i]) ) - 1);
+			temp_a += A[1,2] * (2 * cdf(Normal(0,sigma), (d_b[j]) ) - 1) * (2 * cdf(Normal(0,sigma), (d_a[i]) ) - 1);
 
-			temp_b += A[2,1] * (2 * cdf(Normal(0,sigma), d_a[i]) - 1) * (2 * cdf(Normal(0,sigma),d_b[j]) - 1);
-			temp_b += A[2,2] * (2 * cdf(Normal(0,sigma), d_b[j]) - 1) * (2 * cdf(Normal(0,sigma),d_b[j]) - 1);
+			temp_b += A[2,1] * (2 * cdf(Normal(0,sigma), (d_a[i]) ) - 1) * (2 * cdf(Normal(0,sigma), (d_b[j]) ) - 1);
+			temp_b += A[2,2] * (2 * cdf(Normal(0,sigma), (d_b[j]) ) - 1) * (2 * cdf(Normal(0,sigma), (d_b[j]) ) - 1);
 
 			# Bias from other tasks
 			if(critic_dimensions > 2)
@@ -114,21 +114,22 @@ for i = 1:no_points
 				#=temp_a += (2 * cdf(Normal(0,sigma),d_a[i]) - 1) * (-a_multiplier * R_ext);
 				temp_b += (2 * cdf(Normal(0,sigma),d_b[j]) - 1) * (-a_multiplier * R_ext);=#
 				for(k = 3:critic_dimensions)
-					temp_a += (2 * cdf(Normal(0,sigma),d_a[i]) - 1) * (A[1,k] * R_ext);
-					temp_b += (2 * cdf(Normal(0,sigma),d_b[j]) - 1) * (A[2,k] * R_ext);
+					temp_a += (2 * cdf(Normal(0,sigma), (d_a[i]) ) - 1) * (A[1,k] * R_ext);
+					temp_b += (2 * cdf(Normal(0,sigma), (d_b[j]) ) - 1) * (A[2,k] * R_ext);
 				end
 			end
 
-			#TODO: Multiply by probability of occurence of each task
-			#
+			# Multiply by probability of occurence of each task
+			temp_a *= prob_task[1];
+			temp_b *= prob_task[2];
 
 			# putting it all together
-			deriv_D_a[i,j] = S[1,1] * temp_a + S[1,2] * temp_b;
-			deriv_D_b[i,j] = S[2,1] * temp_a + S[2,2] * temp_b;
+			deriv_D_a_pos[i,j] = ( O[1] * S[1,1] * temp_a + O[2] * S[1,2] * temp_b );
+			deriv_D_b_pos[i,j] = ( O[1] * S[2,1] * temp_a + O[2] * S[2,2] * temp_b );
 
-			#TODO: multiply again by output encoding to give +ve D for success representation
-			#deriv_D_a_pos[i,j] *= O[1];
-			#deriv_D_b_pos[i,j] *= O[2];
+			# multiply again by output encoding to give +ve D for success representation
+			deriv_D_a_pos[i,j] *= O[1];
+			deriv_D_b_pos[i,j] *= O[2];
 		end
 
 		######
@@ -203,7 +204,25 @@ filename_stream = string("stream_",filename_base,".pdf")
 
 if (use_plot_over_D_pos)
 	## Difference in positive outputs view
-	#TODO
+	figure();
+	#streamplot(d_a,d_b,deriv_D_a',deriv_D_b');
+	quiver(d_a,d_b,deriv_D_a_pos',deriv_D_b_pos', units="width", scale=D_pos_scale);
+	xtxt = latexstring("D_1^+");
+	ytxt = latexstring("D_2^+");
+	xlabel(xtxt)
+	ylabel(ytxt) # L"D_2"
+	title("Similarity s=$a");
+	if (critic_dimensions > 2)
+		titletxt = latexstring();
+		title("Similarity s=$a, R_ext = $R_ext, no external processes = $(critic_dimensions-2)");
+	end
+
+	#plot(d_a, Db_null);
+	## x=0 and y=0 lines for visual inspection
+	#=origin = zeros(no_points);
+	origin_space = linspace(-100,100,no_points);
+	plot(origin, origin_space);
+	plot(origin_space, origin);=#
 end
 
 
@@ -211,7 +230,7 @@ if (use_plot_over_D)
 	## Difference in outputs view
 	figure();
 	#streamplot(d_a,d_b,deriv_D_a',deriv_D_b');
-	quiver(d_a,d_b,deriv_D_a',deriv_D_b', units="width", scale=20.0);
+	quiver(d_a,d_b,deriv_D_a',deriv_D_b', units="width", scale=D_scale);
 	xtxt = latexstring("D_1");
 	ytxt = latexstring("D_2");
 	xlabel(xtxt)
@@ -235,7 +254,7 @@ if (use_plot_over_p)
 	## probabilistic view
 	figure();
 	##streamplot(d_a,d_b,deriv_D_a',deriv_D_b');
-	quiver(p,p_y,deriv_p_a',deriv_p_b', units="width", scale=1.0);
+	quiver(p,p_y,deriv_p_a',deriv_p_b', units="width", scale=p_scale);
 	xtxt = latexstring("p_1");
 	ytxt = latexstring("p_2");
 	xlabel(xtxt)
