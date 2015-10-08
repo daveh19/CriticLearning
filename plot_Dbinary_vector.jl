ef9fd89ce02558eabb296e5c30784c7f8b5a0f46
David Higgins (TUPro) <dave@uiginn.com>
2015-10-08 17:53:53 +0200

    Major clean-up of binary outputs code to resemble latest linear outputs code, with optional processing and plotting of different views. The actual calculations still need to be updated.

--- plot_Dbinary_vector.jl
+++ plot_Dbinary_vector.jl
@@ -1,5 +1,6 @@
 using PyPlot;
 using Distributions;
+using LaTeXStrings;
 
 ### Useful functions
 ## There are a number of alternative ways to calculate pdf and cdf inverse
@@ -8,6 +9,13 @@ dist_cdf(x) = cdf(Normal(0,1), x);
 # Note: inv_cdf(x) != 1.0 / cdf(Normal(0,1), x); #Not 1/fn but inverse function!!
 include("inverse_cdf.jl"); #contains invnorm(), consider switching to invphi()
 invphi(p) = sqrt(2) * erfinv(2 * p - 1.0)
+
+
+## Plotting over D, D~ (+ve), and p optional
+use_plot_over_D_pos = true :: Bool;
+use_plot_over_D = false :: Bool;
+use_plot_over_p = true :: Bool;
+use_add_trajectories_to_plot = false :: Bool;
 
 
 ## Space over which vector field is calculated / plotted
@@ -24,6 +32,10 @@ p_y = linspace(0, 1, no_y_points);
 d_a = linspace(-3,3, no_points);
 d_b = linspace(-3,3, no_points);
 
+D_pos_scale = 20.0 :: Float64;
+D_scale = 20.0 :: Float64;
+p_scale = 1.0 :: Float64;
+
 #debug vars
 Da = zeros(no_points);
 Db = zeros(no_y_points);
@@ -35,51 +47,20 @@ p_deriv_D_a = zeros(no_points, no_y_points);
 p_deriv_D_b = zeros(no_points, no_y_points);
 deriv_D_a = zeros(no_points, no_y_points);
 deriv_D_b = zeros(no_points, no_y_points);
+deriv_D_a_pos = zeros(no_points, no_y_points);
+deriv_D_b_pos = zeros(no_points, no_y_points);
 
-
-## Reward learning component to flow is an option in probabilistic diagram
-plot_unbiased_learning = true :: Bool;
-# Success signal is necessary for unbiased learning component
-# constant Success signal
-#S_1 = +1.0 / 10.0;
-#S_2 = -1.0 / 10.0;
-# linear decay of Success signal
-#S_1 = 1.0 - p;
-#S_2 = -p - 1.0;
-# Stationary Success signal
-S_1 = 1.0 - (2*p-1.0);
-S_2 = -1.0 - (2*p-1.0);
-
-## Rho represents error in estimation of Success signal, used in probabilistic calculation
-rho_a = zeros(no_points,no_y_points);
-for i = 1:no_points
-	for j = 1:(no_y_points)
-		rho_a[i,j] = (p[i] - p[j]);
-		#rho_a[i,j] = p[i] - ((p[i] + p[j] + 2.0 * p_ext) / 4.0);
-		#rho_a[i,j] = p[i] - ((p[j] + p_ext) / 2.0);
-	end
-end
-rho_b = zeros(no_points,no_y_points);
-for i = 1:no_points
-	for j = 1:(no_y_points)
-		rho_b[i,j] = (p[j] - p[i]);
-		#rho_b[i,j] = p[j] - ((p[i] + p[j] + 2.0 * p_ext) / 4.0);
-		#rho_b[i,j] = p[j] - ((p[i] + p_ext) / 2.0);
-	end
-end
-
-
-## Constants
-xa_norm_sq = 1.0;
-p_ext = 0.30;
 
 # Confusion parameter
 critic_dimensions = 2;
+# perfect critic (overwritten if any of the following are active)
+C = eye(critic_dimensions)
+#=
+# equal mix critic
 c = 1 / critic_dimensions; # currently equal confusion mix of all true critics
 C = ones(critic_dimensions,critic_dimensions)
 C *= c
-#C = eye(critic_dimensions) # perfect critic
-A = eye(critic_dimensions) - C
+A = eye(critic_dimensions) - C=#
 
 
 # Probabilistic presentation of individual tasks
@@ -95,13 +76,15 @@ A = eye(critic_dimensions) - C;
 
 
 # Input representation similarity parameter
-a = -0.9;
+a = 0.9;
 S = [1 a; a 1]
 
+# Output correlation with +ve D
+O = [1; -1];
+
 # Noise and external bias
-sigma = 1; #sqrt(1); #sqrt(100);
-#rho_ext = -0.5;
-R_ext = 1; #1.001;
+sigma = 1;
+R_ext = 1;
 
 
 for i = 1:no_points
@@ -110,116 +93,91 @@ for i = 1:no_points
 		#
 		# Calculation of change of difference in outputs
 		#
-		# for R^{true} = p
-		# temp_a = sigma^2 * pdf(Normal(0,sigma), d_a[i]);
-		# temp_b = sigma^2 * pdf(Normal(0,sigma), d_b[j]);
-		# # equations for R^{true} = p
-		# temp_a += A[1,1] * cdf(Normal(0,sigma), d_a[i]) * d_a[i];
-		# temp_a += A[1,2] * cdf(Normal(0,sigma), d_b[j]) * d_a[i];
-		# temp_b += A[2,1] * cdf(Normal(0,sigma), d_a[i]) * d_b[j];
-		# temp_b += A[2,2] * cdf(Normal(0,sigma), d_b[j]) * d_b[j];
+		# positive association in plotting of D with reward (use d_a as d_a^~)
+		if (use_plot_over_D_pos)
+			# *2 for R^{true} = (2p-1)
+			temp_a = 4 * cdf(Normal(0,sigma),d_a[i]) * (1 - cdf(Normal(0,sigma),d_a[i]))
+			temp_b = 4 * cdf(Normal(0,sigma),d_b[j]) * (1 - cdf(Normal(0,sigma),d_b[j]))
+			# equations for R^{true} = (2p-1)
+			temp_a += A[1,1] * (2 * cdf(Normal(0,sigma), d_a[i]) - 1) * (2 * cdf(Normal(0,sigma),d_a[i]) - 1);
+			temp_a += A[1,2] * (2 * cdf(Normal(0,sigma), d_b[j]) - 1) * (2 * cdf(Normal(0,sigma),d_a[i]) - 1);
 
-		# *2 for R^{true} = (2p-1)
-		temp_a = 4 * cdf(Normal(0,sigma),d_a[i]) * (1 - cdf(Normal(0,sigma),d_a[i]))
-		temp_b = 4 * cdf(Normal(0,sigma),d_b[j]) * (1 - cdf(Normal(0,sigma),d_b[j]))
-		# equations for R^{true} = (2p-1)
-		temp_a += A[1,1] * (2 * cdf(Normal(0,sigma), d_a[i]) - 1) * (2 * cdf(Normal(0,sigma),d_a[i]) - 1);
-		temp_a += A[1,2] * (2 * cdf(Normal(0,sigma), d_b[j]) - 1) * (2 * cdf(Normal(0,sigma),d_a[i]) - 1);
+			temp_b += A[2,1] * (2 * cdf(Normal(0,sigma), d_a[i]) - 1) * (2 * cdf(Normal(0,sigma),d_b[j]) - 1);
+			temp_b += A[2,2] * (2 * cdf(Normal(0,sigma), d_b[j]) - 1) * (2 * cdf(Normal(0,sigma),d_b[j]) - 1);
 
-		temp_b += A[2,1] * (2 * cdf(Normal(0,sigma), d_a[i]) - 1) * (2 * cdf(Normal(0,sigma),d_b[j]) - 1);
-		temp_b += A[2,2] * (2 * cdf(Normal(0,sigma), d_b[j]) - 1) * (2 * cdf(Normal(0,sigma),d_b[j]) - 1);
-
-		# Bias from other tasks
-		if(critic_dimensions > 2)
-			# a_multiplier assumes equal for all
-			a_multiplier = (critic_dimensions - 2) / critic_dimensions
-			#=temp_a += (2 * cdf(Normal(0,sigma),d_a[i]) - 1) * (-0.5 * R_ext);
-			temp_b += (2 * cdf(Normal(0,sigma),d_b[j]) - 1) * (-0.5 * R_ext);=#
-			#=temp_a += (2 * cdf(Normal(0,sigma),d_a[i]) - 1) * (-a_multiplier * R_ext);
-			temp_b += (2 * cdf(Normal(0,sigma),d_b[j]) - 1) * (-a_multiplier * R_ext);=#
-			for(k = 3:critic_dimensions)
-				temp_a += (2 * cdf(Normal(0,sigma),d_a[i]) - 1) * (A[1,k] * R_ext);
-				temp_b += (2 * cdf(Normal(0,sigma),d_b[j]) - 1) * (A[2,k] * R_ext);
+			# Bias from other tasks
+			if(critic_dimensions > 2)
+				# a_multiplier assumes equal for all
+				a_multiplier = (critic_dimensions - 2) / critic_dimensions
+				#=temp_a += (2 * cdf(Normal(0,sigma),d_a[i]) - 1) * (-0.5 * R_ext);
+				temp_b += (2 * cdf(Normal(0,sigma),d_b[j]) - 1) * (-0.5 * R_ext);=#
+				#=temp_a += (2 * cdf(Normal(0,sigma),d_a[i]) - 1) * (-a_multiplier * R_ext);
+				temp_b += (2 * cdf(Normal(0,sigma),d_b[j]) - 1) * (-a_multiplier * R_ext);=#
+				for(k = 3:critic_dimensions)
+					temp_a += (2 * cdf(Normal(0,sigma),d_a[i]) - 1) * (A[1,k] * R_ext);
+					temp_b += (2 * cdf(Normal(0,sigma),d_b[j]) - 1) * (A[2,k] * R_ext);
+				end
 			end
+
+			#TODO: Multiply by probability of occurence of each task
+			#
+
+			# putting it all together
+			deriv_D_a[i,j] = S[1,1] * temp_a + S[1,2] * temp_b;
+			deriv_D_b[i,j] = S[2,1] * temp_a + S[2,2] * temp_b;
+
+			#TODO: multiply again by output encoding to give +ve D for success representation
+			#deriv_D_a_pos[i,j] *= O[1];
+			#deriv_D_b_pos[i,j] *= O[2];
 		end
 
+		######
+		#
+		# no correction for -ve association in plotting of D with reward (use d_a as d_a)
+		if (use_plot_over_D)
+			#TODO
 
-		# putting it all together
-		deriv_D_a[i,j] = S[1,1] * temp_a + S[1,2] * temp_b;
-		deriv_D_b[i,j] = S[2,1] * temp_a + S[2,2] * temp_b;
+		end
 
 
 		#####
 		#
 		# Calculation of change of probability of outcome
 		#
-		# -ve for task B is for the opposite sign on rho
-		# unbounded post firing rate equations
-		#=deriv_p_a[i,j] = dist_pdf(invnorm(p[i])) * xa_norm_sq * rho[i,j] * invnorm(p[i]);
-		deriv_p_b[i,j] = dist_pdf(invnorm(p[j])) * xa_norm_sq * (-rho[i,j]) * invnorm(p[j]);=#
-		# binary output neurons
-		#=deriv_p_a[i,j] = dist_pdf(invnorm(p[i])) * xa_norm_sq * rho[i,j] * p[i];
-		deriv_p_b[i,j] = - dist_pdf(invnorm(p[j])) * xa_norm_sq * rho[i,j] * p[j];=#
-		# binary output neurons - alternative
-		#=deriv_p_a[i,j] = dist_pdf(invnorm(p[i])) * xa_norm_sq * rho[i,j] * p[i] * (1-p[i]);
-		deriv_p_b[i,j] = - dist_pdf(invnorm(p[j])) * xa_norm_sq * rho[i,j] * p[j] * (1-p[j]);=#
+		if (use_plot_over_p)
+			## the following has not been updates to follow binary output rules yet!!
+			Da[i] = invphi(p[i]);
+			Db[j] = invphi(p_y[j]);
+			p_temp_a = 4 * cdf(Normal(0,sigma),Da[i]) * (1 - cdf(Normal(0,sigma),Da[i]))
+			p_temp_b = 4 * cdf(Normal(0,sigma),Db[j]) * (1 - cdf(Normal(0,sigma),Db[j]))
+			# equations for R^{true} = (2p-1)
+			p_temp_a += A[1,1] * (2 * p[i] - 1) * (2 * p[i] - 1);
+			p_temp_a += A[1,2] * (2 * p[j] - 1) * (2 * p[i] - 1);
 
-		# binary output neurons - new (in first write-up)
-		# effects of unsupervised bias
-		#deriv_p_a[i,j] = dist_pdf(invnorm(p[i])) * xa_norm_sq * rho_a[i,j] * (2 * p[i] - 1);
-		#deriv_p_b[i,j] = dist_pdf(invnorm(p[j])) * xa_norm_sq * rho_b[i,j] * (2 * p[j] - 1);
+			p_temp_b += A[2,1] * (2 * p[i] - 1) * (2 * p[j] - 1);
+			p_temp_b += A[2,2] * (2 * p[j] - 1) * (2 * p[j] - 1);
 
 
-		## the following has not been updates to follow binary output rules yet!!
-		Da[i] = invphi(p[i]);
-		Db[j] = invphi(p_y[j]);
-		p_temp_a = 4 * cdf(Normal(0,sigma),Da[i]) * (1 - cdf(Normal(0,sigma),Da[i]))
-		p_temp_b = 4 * cdf(Normal(0,sigma),Db[j]) * (1 - cdf(Normal(0,sigma),Db[j]))
-		# equations for R^{true} = (2p-1)
-		p_temp_a += A[1,1] * (2 * p[i] - 1) * (2 * p[i] - 1);
-		p_temp_a += A[1,2] * (2 * p[j] - 1) * (2 * p[i] - 1);
-
-		p_temp_b += A[2,1] * (2 * p[i] - 1) * (2 * p[j] - 1);
-		p_temp_b += A[2,2] * (2 * p[j] - 1) * (2 * p[j] - 1);
-
-
-		# Bias from other tasks
-		if(critic_dimensions > 2)
-			a_multiplier = (critic_dimensions - 2) / critic_dimensions
-			#=p_temp_a += (2 * p[i] - 1) * (-0.5 * R_ext);
-			p_temp_b += (2 * p[j] - 1) * (-0.5 * R_ext);=#
-			#=p_temp_a += (2 * p[i] - 1) * (-a_multiplier * R_ext);
-			p_temp_b += (2 * p[j] - 1) * (-a_multiplier * R_ext);=#
-			for(k = 3:critic_dimensions)
-				p_temp_a += (2 * p[i] - 1) * (A[1,k] * R_ext);
-				p_temp_b += (2 * p[j] - 1) * (A[2,k] * R_ext);
+			# Bias from other tasks
+			if(critic_dimensions > 2)
+				a_multiplier = (critic_dimensions - 2) / critic_dimensions
+				#=p_temp_a += (2 * p[i] - 1) * (-0.5 * R_ext);
+				p_temp_b += (2 * p[j] - 1) * (-0.5 * R_ext);=#
+				#=p_temp_a += (2 * p[i] - 1) * (-a_multiplier * R_ext);
+				p_temp_b += (2 * p[j] - 1) * (-a_multiplier * R_ext);=#
+				for(k = 3:critic_dimensions)
+					p_temp_a += (2 * p[i] - 1) * (A[1,k] * R_ext);
+					p_temp_b += (2 * p[j] - 1) * (A[2,k] * R_ext);
+				end
 			end
+
+			# putting it all together
+			p_deriv_D_a[i,j] = S[1,1] * p_temp_a + S[1,2] * p_temp_b;
+			p_deriv_D_b[i,j] = S[2,1] * p_temp_a + S[2,2] * p_temp_b;
+
+			deriv_p_a[i,j] = pdf(Normal(0,sigma), Da[i]) * p_deriv_D_a[i,j];
+			deriv_p_b[i,j] = pdf(Normal(0,sigma), Db[j]) * p_deriv_D_b[i,j];
 		end
-
-		# putting it all together
-		p_deriv_D_a[i,j] = S[1,1] * p_temp_a + S[1,2] * p_temp_b;
-		p_deriv_D_b[i,j] = S[2,1] * p_temp_a + S[2,2] * p_temp_b;
-
-		deriv_p_a[i,j] = pdf(Normal(0,sigma), Da[i]) * p_deriv_D_a[i,j];
-		deriv_p_b[i,j] = pdf(Normal(0,sigma), Db[j]) * p_deriv_D_b[i,j];
-
-
-		#=if(plot_unbiased_learning)
-			# include effects of regular learning signal
-			#deriv_p_a[i,j] = dist_pdf(invnorm(p[i])) * xa_norm_sq * ( ( p[i]*S_1[i] - (1-p[i])*S_2[i] ) + ( rho_a[i,j] * (2 * p[i] - 1) ) );
-			#deriv_p_b[i,j] = dist_pdf(invnorm(p[j])) * xa_norm_sq * ( ( p[j]*S_1[j] - (1-p[j])*S_2[j] ) + ( rho_b[i,j] * (2 * p[j] - 1) ) );
-			deriv_p_a[i,j] += dist_pdf(invnorm(p[i])) * xa_norm_sq * ( ( p[i]*S_1[i] - (1-p[i])*S_2[i] ) );
-			deriv_p_b[i,j] += dist_pdf(invnorm(p[j])) * xa_norm_sq * ( ( p[j]*S_1[j] - (1-p[j])*S_2[j] ) );
-		end=#
-		# wta binary output for principal choice, second choice is normalised by magnitude of first
-		#=deriv_p_a[i,j] = dist_pdf(invnorm(p[i])) * xa_norm_sq * rho[i,j] * (p[i] + (1-p[i]) * p[i]);
-		deriv_p_b[i,j] = - dist_pdf(invnorm(p[j])) * xa_norm_sq * rho[i,j] * (p[j] + (1-p[j]) * p[j]);=#
-		# Rescaled outputs - new (in first write-up)
-		#=deriv_p_a[i,j] = dist_pdf(invnorm(p[i])) * xa_norm_sq * rho[i,j] * ( (2 * p[i] - 1) * (1 - p[i]) );
-		deriv_p_b[i,j] = dist_pdf(invnorm(p[j])) * xa_norm_sq * (-rho[i,j]) * ( (2 * p[j] - 1) * (1 - p[j]) );=#
-		# basic flow field
-		#deriv_p_a[i,j] = xa_norm_sq * rho[i,j] * (2 * p[i] - 1);
-		#deriv_p_b[i,j] = xa_norm_sq * (-rho[i,j]) * (2 * p[j] - 1);
 	end
 end
 
@@ -243,41 +201,53 @@ filename_stream = string("stream_",filename_base,".pdf")
 #streamplot(p,p_y,deriv_p_a',deriv_p_b')
 #savefig(filename_stream);
 
-figure();
-#streamplot(d_a,d_b,deriv_D_a',deriv_D_b');
-quiver(d_a,d_b,deriv_D_a',deriv_D_b', units="width", scale=20.0);
-xtxt = latexstring("D_1");
-ytxt = latexstring("D_2");
-xlabel(xtxt)
-ylabel(ytxt) # L"D_2"
-title("Similarity s=$a");
-if (critic_dimensions > 2)
-	titletxt = latexstring();
-	title("Similarity s=$a, R_ext = $R_ext, no external processes = $(critic_dimensions-2)");
+if (use_plot_over_D_pos)
+	## Difference in positive outputs view
+	#TODO
 end
 
-#plot(d_a, Db_null);
-## x=0 and y=0 lines for visual inspection
-#=origin = zeros(no_points);
-origin_space = linspace(-100,100,no_points);
-plot(origin, origin_space);
-plot(origin_space, origin);=#
 
-figure();
-##streamplot(d_a,d_b,deriv_D_a',deriv_D_b');
-quiver(p,p_y,deriv_p_a',deriv_p_b', units="width", scale=1.0);
-xtxt = latexstring("p_1");
-ytxt = latexstring("p_2");
-xlabel(xtxt)
-ylabel(ytxt) # L"D_2"
-aa = abs(a);
-title("Similarity s=$aa");
-if (critic_dimensions > 2)
-	titletxt = latexstring();
-	title("Similarity s=$aa, R_ext = $R_ext, no external processes = $(critic_dimensions-2)");
+if (use_plot_over_D)
+	## Difference in outputs view
+	figure();
+	#streamplot(d_a,d_b,deriv_D_a',deriv_D_b');
+	quiver(d_a,d_b,deriv_D_a',deriv_D_b', units="width", scale=20.0);
+	xtxt = latexstring("D_1");
+	ytxt = latexstring("D_2");
+	xlabel(xtxt)
+	ylabel(ytxt) # L"D_2"
+	title("Similarity s=$a");
+	if (critic_dimensions > 2)
+		titletxt = latexstring();
+		title("Similarity s=$a, R_ext = $R_ext, no external processes = $(critic_dimensions-2)");
+	end
+
+	#plot(d_a, Db_null);
+	## x=0 and y=0 lines for visual inspection
+	#=origin = zeros(no_points);
+	origin_space = linspace(-100,100,no_points);
+	plot(origin, origin_space);
+	plot(origin_space, origin);=#
 end
 
-sub_task_id_to_plot = 1;
+
+if (use_plot_over_p)
+	## probabilistic view
+	figure();
+	##streamplot(d_a,d_b,deriv_D_a',deriv_D_b');
+	quiver(p,p_y,deriv_p_a',deriv_p_b', units="width", scale=1.0);
+	xtxt = latexstring("p_1");
+	ytxt = latexstring("p_2");
+	xlabel(xtxt)
+	ylabel(ytxt) # L"D_2"
+	aa = abs(a);
+	title("Similarity s=$aa");
+	if (critic_dimensions > 2)
+		titletxt = latexstring();
+		title("Similarity s=$aa, R_ext = $R_ext, no external processes = $(critic_dimensions-2)");
+	end
+end
+
 
 function add_trajectories_to_linear_p_plot(latest_experiment_results, sub_task_id)
 	include("parameters_critic_simulations.jl"); # dont' change the paramters in between calls!
@@ -307,4 +277,8 @@ function add_trajectories_to_linear_p_plot(latest_experiment_results, sub_task_i
 	axis([-0.005,1.005,-0.005,1.005]);
 end
 
-add_trajectories_to_linear_p_plot(exp_results[1],sub_task_id_to_plot);
+
+if (use_plot_over_p && use_add_trajectories_to_plot)
+	sub_task_id_to_plot = 1;
+	add_trajectories_to_linear_p_plot(exp_results[1],sub_task_id_to_plot);
+end
