4555a3eb653c5e5c4d475f0c8a2fa5d8e943ff12
David Higgins (TUPro) <dave@uiginn.com>
2015-10-21 10:02:38 +0200

    Separated add_trajectories_to_p_plot() into own file so it can be written just once for each calling script.

--- plot_Dbinary_vector.jl
+++ plot_Dbinary_vector.jl
@@ -9,6 +9,7 @@ dist_cdf(x) = cdf(Normal(0,1), x);
 # Note: inv_cdf(x) != 1.0 / cdf(Normal(0,1), x); #Not 1/fn but inverse function!!
 include("inverse_cdf.jl"); #contains invnorm(), consider switching to invphi()
 invphi(p) = sqrt(2) * erfinv(2 * p - 1.0)
+include("plotting_assist_functions.jl");
 
 
 ## Plotting over D, D~ (+ve), and p optional
@@ -333,46 +334,6 @@ if (use_plot_over_p)
 		titletxt = latexstring();
 		title("Similarity s=$aa, R_ext = $R_ext, no external processes = $(critic_dimensions-2)");
 	end
-end
-
-
-
-function add_trajectories_to_linear_p_plot(latest_experiment_results, sub_task_id)
-	include("parameters_critic_simulations.jl"); # don't change the paramters in between calls!
-
-	for j = 1:no_subjects
-		local_prop_sub_1_correct = zeros(no_blocks_in_experiment);
-		local_prop_sub_2_correct = zeros(no_blocks_in_experiment);
-		for i = 1:no_blocks_in_experiment
-			#scatter(latest_experiment_results.subjects_task[j,sub_task_id].blocks[i].proportion_task_correct[1], latest_experiment_results.subjects_task[j,sub_task_id].blocks[i].proportion_task_correct[2], marker="o", c="c")
-			if(use_plot_measured_proportion_correct)
-				local_prop_sub_1_correct[i] = latest_experiment_results.subjects_task[j,sub_task_id].blocks[i].proportion_task_correct[1];
-				local_prop_sub_2_correct[i] = latest_experiment_results.subjects_task[j,sub_task_id].blocks[i].proportion_task_correct[2];
-			else
-				local_prop_sub_1_correct[i] = latest_experiment_results.subjects_task[j,sub_task_id].blocks[i].probability_correct[sub_task_id,1];
-				local_prop_sub_2_correct[i] = latest_experiment_results.subjects_task[j,sub_task_id].blocks[i].probability_correct[sub_task_id,2];
-			end
-		end
-		plot(local_prop_sub_1_correct, local_prop_sub_2_correct, "r", zorder=1)
-		#print("",local_prop_sub_1_correct, local_prop_sub_2_correct, "\n-----\n")
-	end
-	for j = 1:no_subjects
-		for i = 1:no_blocks_in_experiment
-			if(use_plot_measured_proportion_correct)
-				# start point
-				scatter(latest_experiment_results.subjects_task[j,sub_task_id].blocks[1].proportion_task_correct[1], latest_experiment_results.subjects_task[j,sub_task_id].blocks[1].proportion_task_correct[2], marker="s", c="r", s=40, zorder=2)
-				# end point
-				scatter(latest_experiment_results.subjects_task[j,sub_task_id].blocks[end].proportion_task_correct[1], latest_experiment_results.subjects_task[j,sub_task_id].blocks[end].proportion_task_correct[2], marker="D", c="g", s=60, zorder=3)
-			else
-				# start point
-				scatter(latest_experiment_results.subjects_task[j,sub_task_id].blocks[1].probability_correct[sub_task_id,1], latest_experiment_results.subjects_task[j,sub_task_id].blocks[1].probability_correct[sub_task_id,2], marker="s", c="r", s=40, zorder=2)
-				# end point
-				scatter(latest_experiment_results.subjects_task[j,sub_task_id].blocks[end].probability_correct[sub_task_id,1], latest_experiment_results.subjects_task[j,sub_task_id].blocks[end].probability_correct[sub_task_id,2], marker="D", c="g", s=60, zorder=3)
-			end
-		end
-	end
-
-	axis([-0.005,1.005,-0.005,1.005]);
 end
 
 
