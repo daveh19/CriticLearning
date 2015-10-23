87fe15831cf79af68f31c8593b789e3db54f4e8c
David Higgins (Air II) <dave@uiginn.com>
2015-10-23 14:37:54 +0200

    Minor changes to scripted overlay of trajectories from biased simulation onto individualised flow fields, probably still not correct.

--- script_check_S.jl
+++ script_check_S.jl
@@ -1,21 +1,36 @@
-## load exprimental candidates and calculate their similarity matrices
+## load experimental candidates and calculate their similarity matrices
 # usage: Run a simulation of the roving experiment first.
 #   then run this script
 #   if S is commented out in plot_D_vector.jl then it will plot the
 #   accurate flow fields for each simulation in turn
 
 task_id = 2;
-use_linear_out = false :: Bool;
+exp_id = 2; # only for biased roving simulation
+use_linear_out = true :: Bool;
+use_roving_subjects = false :: Bool;
+
+# set critic_dimensions and
+# comment out S and
+# disable trajectories and
+# choose measured vs perfect probabilites
+#	 in the plotting file!
 
 include("plotting_assist_functions.jl");
 
 for i=1:no_subjects
-  restore_subject(exp_results[1].subjects_task[i,task_id], false);
+	if (!use_roving_subjects)
+  	restore_subject(exp_results[1].subjects_task[i, task_id], false);
+	else
+		task_id = exp_id;
+		restore_subject(exp_results[1].subjects_roving_task[i, exp_id], false);
+	end
   pos = sum( pre(1.0,task_id,linear_tc()) .* pre(1.0, task_id, linear_tc()) );
   neg = sum( pre(-1.0,task_id,linear_tc()) .* pre(-1.0, task_id, linear_tc()) );
   pn = sum( pre(1.0,task_id,linear_tc()) .* pre(-1.0, task_id, linear_tc()) );
 
-  S = [pos pn; pn neg];
+	global a = pn;
+  global S = [pos pn; pn neg];
+	print("",S,"\n")
   S /= S[1,1];
   #S[1,:] /= S[1,1];
   #S[2,:] /= S[2,2];
@@ -26,5 +41,9 @@ for i=1:no_subjects
   else
     include("plot_Dbinary_vector.jl")
   end
-  add_specific_trajectory_to_linear_p_plot(exp_results[1],task_id, i);
+	if (!use_roving_subjects)
+  	add_specific_trajectory_to_linear_p_plot(exp_results[1],task_id, i);
+	else
+		add_specific_roving_trajectory_to_linear_p_plot(exp_results[1],exp_id, i);
+	end
 end
