e8b951de3df635aaf8b65b596288e2e923221131
David Higgins (TUPro) <dave@uiginn.com>
2017-02-15 14:27:06 +0100

    Debugging Kalman approach. Tried adding Inf to the observation noise model. Doesn't work. Moved data generation to new positive mean values. Added scatter points in plotting.

--- kalman_tracking.jl
+++ kalman_tracking.jl
@@ -8,7 +8,8 @@ function generate_two_reward_sequences(sequence_length = 100, noise_sigma = 0.1)
 
   for i = 1:sequence_length
     sequence_id[i] = round(Int,(rand(Uniform(0,1)) < 0.5 ? 1 : 2));
-    sequence_value[i] = sequence_id[i] - 1.5 + rand(Normal(0,1)) .* noise_sigma;
+    sequence_value[i] = (sequence_id[i]*2) - 1.5 + rand(Normal(0,1)) .* noise_sigma;
+    # sequence_value[i] = 3.0 + rand(Normal(0,1)) .* noise_sigma;
     element_count[round(Int,sequence_id[i])] += 1;
   end
 
@@ -23,6 +24,7 @@ function generate_two_reward_sequences(sequence_length = 100, noise_sigma = 0.1)
   y2 = sequence_value[sequence_id .== 2];
   plot(x1, y1, "r", linewidth=2, label="Task 1 R signal")
   plot(x2, y2, "b", linewidth=2, label="Task 2 R signal")
+  scatter(linspace(1,100,sequence_length), sequence_value);
 
   return [sequence_id sequence_value];
 end
@@ -42,8 +44,8 @@ function kalman_host()
   # tracking_corrected_error_covariance = zeros(2,no_data_points);
 
   k_dict = kalman_initialise(0.99);
-  process_noise_model = 0.00;
-  sigma_1_sq = sigma_2_sq = 0.1;
+  process_noise_model = 0.01;
+  sigma_1_sq = sigma_2_sq = 0.3;
   observation_noise_model = [sigma_1_sq 0 ; 0 sigma_2_sq];
 
   data_matrix = generate_two_reward_sequences(no_data_points);
@@ -78,14 +80,28 @@ end
 
 
 function kalman_update_correction(k_dict, data_row, observation_noise_model)
+  local_observation_noise_model = deepcopy(observation_noise_model);
   task_id = data_row[1];
   reward_value = data_row[2];
   observed_reward = zeros(2,1);
   observed_reward[round(Int,task_id)] = reward_value;
 
+  # Hack, try combining monitors to make a single (dual) prediction for testing
+  # observed_reward[1] = reward_value;
+  # observed_reward[2] = reward_value;
+
+  if task_id == 1
+    local_observation_noise_model[2,2] = Inf
+  elseif task_id == 2
+    local_observation_noise_model[1,1] = Inf
+  else
+    print("\nERROR: this shouldn't happen\n");
+  end
+
   print("\n", observed_reward);
 
-  K = k_dict["updated_error_covariance"] * (k_dict["updated_error_covariance"] + observation_noise_model)^(-1);
+  K = k_dict["updated_error_covariance"] * inv(k_dict["updated_error_covariance"] + local_observation_noise_model);
+  print("\nK ", K);
   k_dict["corrected_reward_estimate"] = k_dict["updated_reward_estimate"] + K * (observed_reward - k_dict["updated_reward_estimate"]);
   k_dict["corrected_error_covariance"] = (1 - K) * k_dict["updated_error_covariance"];
 end
