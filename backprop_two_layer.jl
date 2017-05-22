abec0ebcbdf4d29e742796e38249b9d5b0e2d8b3
David Higgins (TUPro) <dave@uiginn.com>
2017-05-22 16:32:34 +0200

    Backprop critic representation functions better when reward is in range [0,1] rather than [-1,1] so I'm internally converting between the two now, the internal representation is on [0,1].

--- backprop_two_layer.jl
+++ backprop_two_layer.jl
@@ -19,7 +19,7 @@ function initialise_critic_parameters()
   # srand(2); # use with care, it's being used elsewhere in my simulations
 
   ## Layer 1
-  W1 = ones(2,2) * 0.5;
+  W1 = ones(2,2) * 0.25;
   stdev_weight_noise = 0.; #0.001;
   weight_noise = rand(Normal(0,1),2,2) * stdev_weight_noise;
   # weight_noise = zeros(2,2);
@@ -30,7 +30,7 @@ function initialise_critic_parameters()
   W1 += weight_noise;
 
   ## Layer 2
-  W2 = ones(2,1) #+ rand(Normal(0,1),2,1) * 0.001;
+  W2 = ones(2,1) / 2 #+ rand(Normal(0,1),2,1) * 0.001;
   # Playing with difference in weight initialisation values
   # W2[1] = 3;
   # W2[2] = 0.3;
@@ -88,17 +88,26 @@ function get_output(x, W1, W2)
 end
 
 
-function modify_W!(x, y, z, target, W1, W2, use_realistic_feedback::Bool=false)
+function modify_W!(x, y, z, target, W1, W2, use_realistic_feedback::Bool=false, change_reward_range::Bool=false)
   # alpha = [1.; 1.];
   # tau = [500; 5];
   # tau = [500; 30];
   alpha = my_critic.alpha;
   tau = my_critic.tau;
 
+  if change_reward_range
+    # convert reward from [-1,+1] to [0,1] internal representation (it's smoother)
+    target = (target / 2.) + 0.5;
+  end
+
   if use_realistic_feedback
     # use contingency to generate probabilistic feedback signal
     probability_target = target;
     feedback = ( rand(Uniform(0,1)) < probability_target ? 1 : 0);
+    # this code is for debugging, in the backprop host code it originally generated locally a
+    #   feedback of {0,1}, in the sim we typically provide feedback of {-1,+1} so
+    #   we're briefly going to generate that locally here... for debugging.
+    # feedback = ( rand(Uniform(0,1)) < probability_target ? 1 : -1);
     # @show feedback
     target = feedback;
   end
@@ -138,34 +147,47 @@ function modify_W!(x, y, z, target, W1, W2, use_realistic_feedback::Bool=false)
 end
 
 
-function update_critic_representation(task_id::Int, local_reward::Float64) # later can make Int of local_reward
+function update_critic_representation(task_id::Int, local_reward::Float64, change_reward_range::Bool=false) # later can make Int of local_reward
   # needs access to x (from get_inputs(task_id)), W1, W2
   x = get_inputs(task_id);
   y = get_output(x, my_critic.W1);
   z = get_output(x, my_critic.W1, my_critic.W2);
 
+  if change_reward_range
+    # convert reward from [-1,+1] to [0,1] internal representation (it's smoother)
+    local_reward = (local_reward / 2.) + 0.5;
+  end
+
   modify_W!(x, y, z, local_reward, my_critic.W1, my_critic.W2, false);
 end
 
-function update_critic_representation(task_id::Int, local_reward::Int)
+function update_critic_representation(task_id::Int, local_reward::Int, change_reward_range::Bool=false)
   # use the Float64 version of this function for now
-  update_critic_representation(task_id, float(local_reward));
+  update_critic_representation(task_id, float(local_reward), change_reward_range);
 end
 
 
-function get_reward_prediction(task_id::Int)
+function get_reward_prediction(task_id::Int, change_reward_range::Bool=false)
   # needs access to x (from get_inputs(task_id)) and W1, W2
   x = get_inputs(task_id);
   rp = get_output(x, my_critic.W1, my_critic.W2);
-  return rp;
+
+  if change_reward_range
+    # convert to [-1,+1] scale
+    ret_val = (rp - 0.5) * 2;
+  else
+    ret_val = rp;
+  end
+
+  return ret_val;
 end
 
 
-function run_matrix(realistic_feedback::Bool=false)
+function run_matrix(realistic_feedback::Bool=false, change_reward_range::Bool=false)
   no_trials = 6000;
-  initial_contingency = [0.8; 0.5];
+  initial_contingency = [0.; 0.5];
   switch_point = 3000;
-  second_contingencies = [1.0; 0.2];
+  second_contingencies = [-0.7; 0.2];
 
   (task_sequence, W1, W2) = initialise_critic_sim(no_trials);
 
@@ -192,11 +214,11 @@ function run_matrix(realistic_feedback::Bool=false)
     end
 
     if i < switch_point
-      modify_W!(x,y,z,initial_contingency[task_sequence[i]],W1,W2,realistic_feedback);
-      # update_critic_representation(task_sequence[i], initial_contingency[task_sequence[i]]);
+      modify_W!(x,y,z,initial_contingency[task_sequence[i]],W1,W2,realistic_feedback,change_reward_range);
+      # update_critic_representation(task_sequence[i], initial_contingency[task_sequence[i]], change_reward_range);
     else
-      modify_W!(x,y,z,second_contingencies[task_sequence[i]],W1,W2,realistic_feedback);
-      # update_critic_representation(task_sequence[i], second_contingencies[task_sequence[i]]);
+      modify_W!(x,y,z,second_contingencies[task_sequence[i]],W1,W2,realistic_feedback,change_reward_range);
+      # update_critic_representation(task_sequence[i], second_contingencies[task_sequence[i]], change_reward_range);
     end
 
     @show W1 W2 task_sequence[i]
